dfe24ff539f3845a59dcfbae94c6eb7028ad1e3c
jane181 <39273502+jane181@users.noreply.github.com>
2022-02-23 19:24:52 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
 public class main {
-    public static void main(String[] args) {
-        System.exit(1);
+   public static void main(String[] args) {
+        int flag=0;
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            String s =input.nextLine();
+    String[] newStr = s.split("\\s+");
+    for (int i = 0; i < newStr.length; i++) {
+
+        if (newStr[i].equals("int")) {
+            newStr[i] = "define";
+        } else if (newStr[i].equals("main()")) {
+            newStr[i] = "dso_local i32 @main()";
+        } else if (newStr[i].equals("{")) {
+            newStr[i] = "{";
+        } else if (newStr[i].equals("return")) {
+            newStr[i] = "ret";
+            flag = 1;
+        } else if (newStr[i].equals(";")) {
+            newStr[i] = " ";
+            flag = 0;
+        }
+//        else {
+//            for (int l = newStr[i].length(); --i >= 0; ) {
+//                int chr = newStr[i].charAt(i);
+//                newStr[i] = "";
+//                if (chr==32){
+//                    continue;
+//                }
+//                if (chr >= 48 && chr <= 57) {
+//                    newStr[i] += chr;
+//                } else if (chr == 59) {
+//                    flag = 0;
+//                    System.out.print("    ");
+//                    break;
+//                }
+//
+//            }
+//        }
+
     }
+            if (flag == 1) {
+                System.exit(1);
+            }
+        for (int i = 0; i < newStr.length; i++) {
+            System.out.print(newStr[i] + " ");
+        }
+        System.out.print("\n");
+    }
+}
 }
